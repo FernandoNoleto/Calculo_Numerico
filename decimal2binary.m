8b4e922c3cc25d7e3106cef8496b6b5176f7ab97
FernandoNoleto <fernandoleto@hotmail.com>
2018-08-07 17:27:28 -0300

    Algoritmo de conversão: Decimal->Binário

---
+++ decimal2binary.m
@@ -0,0 +1,17 @@
+n = input('Digite um número:. ');
+q = fix(n/2);
+r = n - (2 * q);
+
+result = [r];
+
+while q != 0
+
+  %result(end+1) = r
+  n = q;
+  q = fix(n/2);
+  r = n - (2 * q);
+  result(end+1) = r;
+endwhile
+
+disp('Representação final:. ');
+disp(fliplr(result));
